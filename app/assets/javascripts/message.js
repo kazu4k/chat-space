ad7066f7688e1c524990f6636d2eba1f7c0b4540
kazu4k <57794695+kazu4k@users.noreply.github.com>
2019-12-03 19:49:36 +0900

    create-auto_reroud

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,76 @@
-$(function(){
+$(function () {
+
   function buildHTML(message) {
-    var content = message.content ? `${ message.content }` : "";
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="main__space__box" data-message-id="${message.id}">
-                  <div class="main__space__thread">
-                    <p class="main__space__name">
-                      ${message.user_name}
-                    </p>
-                    <p class="main__space__date">
-                      ${message.date}
-                    </p>
-                  </div>
-                  <p class="main__space__text">
-                    <div>
-                    ${content}
+
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
+
+      var html = `<div class="main__space__box" data-message-id="${message.id}">
+                    <div class="main__space__thread">
+                      <p class="main__space__name">
+                        ${message.user_name}
+                      </p>
+                      <p class="main__space__date">
+                        ${message.date}
+                      </p>
                     </div>
-                    ${img}
-                  </p>
-                </div>`
+                    <p class="main__space__text">
+                      <div>
+                      ${message.content}
+                      </div>
+                      ${image}
+                    </p>
+                  </div>`
   return html;
   }
-  function scrollFunction () {
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-  };
-  $('#new_message').on('submit', function(e){
-    e.preventDefault(this);
-    var message = new FormData(this);
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: 'POST',
-      data: message,
+      data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
     })
-    .done(function(data){
-      var html = buildHTML(data);
+    .done(function(message) {
+      var html = buildHTML(message);
       $('.main__space').append(html);
-      $('form')[0].reset();
-      $(".main__space").scrollTop( $(".main__space")[0].scrollHeight );
-    })
-    .fail(function(data){
-      alert('メッセージ送信に失敗しました。');
-    })
-    .always(function(data){
+      $('#new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
+      $('.main__space').animate({ scrollTop: $('.main__space')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
     })
   })
+
+  $(function() {
+    var reloadMessages = function() {
+    var href = 'api/messages'
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        last_message_id = $('.main__space__box:last').data("message-id");
+        $.ajax({
+          url: href,
+          type: 'GET',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          var insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML += buildHTML(message);
+            $('.main__space').append(insertHTML);
+        })
+        $('.main__space').animate({scrollTop: $('.main__space')[0].scrollHeight}, 'fast');
+      })
+        .fail(function() {
+          alert('自動更新に失敗しました');
+      });
+    };
+  };
+  setInterval(reloadMessages, 7000);
+  });
 });
